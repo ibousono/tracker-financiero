424754939a698b709588baff426bb94ab605afb8
Ignacio Bousoño <bousonoignacio@gmail.com>
2026-01-15 01:04:30 -0300

    agregue que la ventana arranque maximizada

--- setup/ventana.py
+++ setup/ventana.py
@@ -3,7 +3,7 @@ import tkinter as tk
 def configurarVentana(app):
     app.ventana = tk.Tk()
     app.ventana.title("💰 Tracker Financiero Personal")
-    app.ventana.geometry("1000x700")
+    app.ventana.state("zoomed") 
     app.ventana.configure(bg="#121212")
     
     # Hacer la ventana un poco responsiva
